c70e1a5bb56b3401845319a73e6f26f70f97850c
Geordie <geordiem@student.unimelb.edu.au>
2024-05-18 17:58:02 +1000

    adding select into the mix of forms

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,37 @@
 // Adding styling to fillable text forms
-function textFormClicked(div) {
-	// Creating variables for the elements inside the div
-	const label = div.querySelector("label");
-	const input = div.querySelector("input");
+function textFormClicked(clickedElement, clickedElementType) {
+	// The clickedElement can either be some kind of input (e.g., input, select, etc.) or the div that contains this input and the label. So, the first thing this function does is assign the correct definition of what the variable 'div' should be
+	let div;
+	if (clickedElementType == "div") {
+		// If it's a div that's just been clicked on, then the div is just this element
+		div = clickedElement;
+	} else {
+		// If it's an input type that's been clicked on, then the div we want is a grandparent of the input (since the input is wrapped in a div of its own which is a sibling of the label and child of the top-level div)
+		div = clickedElement.parentNode.parentNode;
+	}
 
+	// Working out what kind of input this field has
+	if (div.querySelector("select")) {
+		var inputType = "select";
+	} else {
+		var inputType = "input";
+	}
+
+	// Creating variables for the various elements of the form
+	const label = div.querySelector("label");
+	const input = (inputType == "select") ? 
+		div.querySelector("select") : 
+		div.querySelector("input");
+	
 	// Assigning style classes to the label and input to resize them and bring the input into view
 	classAdd( label , "input--not-blank" );
 	classAdd( input , "input--not-blank" );
 
 	// Putting the user's cursor and focus into the input so they can automatically start typing
 	input.focus();
+	if (inputType == "select") {
+		input.open();
+	}
 
 	// Adding an event listener for when the user clicks off/unfocuses this form
 	on(input, "focusout", () => {
